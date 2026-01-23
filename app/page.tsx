b9609d7427ea4528e9db58dc2a25be39b591a14e
Edwin Castro <edwin.castro@agenciamatch.cl>
2026-01-23 00:30:33 -0500

    feat: change max width layout :sparkles:

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import TrendsBar from "./components/TrendsBar/TrendsBar";
 import Logo from "./components/Logo/Logo";
 import {
@@ -22,7 +21,7 @@ import NewsCarousel from "./components/NewsCarousel/NewsCarousel";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-6xl flex-col px-4 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-7xl flex-col px-4 bg-white dark:bg-black sm:items-start">
         <Logo />
 
         <TrendsBar />
