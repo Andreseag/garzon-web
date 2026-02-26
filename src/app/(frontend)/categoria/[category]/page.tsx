cc6fc094882ed795cf58a56c193078780dcff6c9
Edwin Castro <edwin.castro@agenciamatch.cl>
2026-02-25 22:39:44 -0500

    feat: test :sparkles:

--- src/app/(frontend)/categoria/[category]/page.tsx
+++ src/app/(frontend)/categoria/[category]/page.tsx
@@ -1,6 +1,6 @@
 import { getPayload } from 'payload'
 import configPromise from '@payload-config'
-// import { notFound } from 'next/navigation'
+import { notFound } from 'next/navigation'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Media, News } from '@/payload-types'
