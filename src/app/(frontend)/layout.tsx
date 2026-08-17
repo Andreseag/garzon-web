6e798e74112edfcf0978285c62306042f38518ee
Edwin Castro <edwin.castro@agenciamatch.cl>
2026-08-17 08:21:42 -0500

    feat: hidde modal :sparkles:

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -83,7 +83,7 @@ export default async function RootLayout({
           <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <main className="flex min-h-screen w-full max-w-7xl flex-col px-4 bg-white dark:bg-black sm:items-start">
               <Analytics />
-              <DefaultModal />
+              {/* <DefaultModal /> */}
 
               {/* SLIDER DE PUBLICIDAD: Ubicado en la parte superior absoluta, sobre el logo/header */}
               <GlobalPromoSlider promos={promos} />
